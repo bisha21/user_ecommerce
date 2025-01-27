1868aa802f589efe9468789e142ea93b81948fc3
[bisha21] <[timilsinab22bi@gmail.com]>
2025-01-27 22:00:45 +0545

    `Added vite-plugin-eslint and updated dependencies in package-lock.json and package.json`

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load environment variables (prefixed with VITE_)
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    base: '/', // Serve from the root
-    build: {
-      outDir: 'dist', // Output directory for production build
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
